b29e43786a1a8cef348d148858148a6e9d64be03
John Brewer <brewerja@gmail.com>
2018-10-10 00:25:27 -0400

    Polish up the pitcher positioning

--- app/static/helper.js
+++ app/static/helper.js
@@ -9,72 +9,96 @@ $(window).on('load', function() {
 
 function setSVGHeight() {
     const Y_BUFFER = 30;
-    let boxHeight = $(svg).find('#away_team').find('rect')[0].height.baseVal.value
-    let maxNameY = Math.max(...getNamesY('.home-pitcher-name').concat(getNamesY('.away-pitcher-name')))
+    const boxHeight = $(svg).find('#away_team').find('rect')[0].height.baseVal.value
+    const maxNameY = Math.max(...getNamesY('home').concat(getNamesY('away')))
     $(svg).attr('height', Math.max(boxHeight, maxNameY) + Y_BUFFER);
 }
 
-function getNamesY(nameClass) {
-    return $(svg).find(nameClass).toArray().map(name => name.getBBox().y + name.getBBox().width / 2)
+function getNamesY(side) {
+    return getNames(side).map(name => name.y.baseVal[0].value + name.getBBox().width / 2)
 }
 
-function getHashes(hashClass) {
-    return $(svg).find(hashClass).toArray().map(line => line.y1.baseVal.value)
-}
-
-function getHashObjects(hashClass) {
-    return $(svg).find(hashClass).toArray()
+function getNames(side) {
+    return $(svg).find('.' + side + '-pitcher-name').toArray()
 }
 
 function adjustPitcherNames(side) {
-    const BUFFER = 40
-    let hashes = getHashes('.' + side + '-pitcher-hash')
-    let hashObjects = $(svg).find('.' + side + '-pitcher-hash').toArray()
-    let names = $(svg).find('.' + side + '-pitcher-name').toArray()
-    for (let i = 0; i < names.length; i++) {
-        let name = names[i]
-        let nameBox = name.getBBox()
+    const hashes = getHashes(side)
+    const names = getNames(side)
 
-        if (nameBox.y - nameBox.width / 2 < hashes[i]) {
-            var level = 1
-            for (let j = 0; j < i; j++) {
-                let pBox = names[j].getBBox()
-                var xCollide
-                if (name === 'home')
-                    xCollide = pBox.x - pBox.height / 2 > nameBox.x
-                else
-                    xCollide = pBox.x - pBox.height / 2 < nameBox.x
-                let yCollide = pBox.y + pBox.width / 2 + 10 > nameBox.y - nameBox.width / 2
-                if (xCollide && yCollide)
-                    level += 1
-            }
-            y = name.y.baseVal[0].value
-            if (side === 'home') {
-                newX = name.x.baseVal[0].value + nameBox.height * level
-                $(name).attr('x', newX)
-                $(name).attr('transform', 'rotate(90,' + newX + ',' + y + ')')
-            } else {
-                newX = name.x.baseVal[0].value - nameBox.height * level
-                $(name).attr('x', newX)
-                $(name).attr('transform', 'rotate(-90,' + newX + ',' + y + ')')
-            }
-        } else if (hashes[i + 1] - hashes[i] - nameBox.width > BUFFER) {
-            // Draw lines between hashes
-            let x = (hashObjects[i].x1.baseVal.value + hashObjects[i].x2.baseVal.value) / 2
-            var transform
-            if (side === 'home')
-                transform = 'translate(0, 0) scale(1, 1)'
-            else
-                transform = 'translate(600, 0) scale(-1, 1)'
-            let y1 = hashObjects[i].y1.baseVal.value
-            let y2 = nameBox.y - nameBox.width / 2 + 5
-            appendLine(x, y1, x, y2, transform, 'team-box')
-            let y3 = nameBox.y + nameBox.width / 2 + 10
-            let y4 = hashObjects[i + 1].y1.baseVal.value
-            appendLine(x, y3, x, y4, transform, 'team-box')
+    for (const [i, name] of names.entries()) {
+        if (isNameWiderThanAdjacentHashMarks(name, hashes[i])) {
+
+            const newX = getNewX(names, i, side)
+            shiftName(name, side, newX)
+
+        } else if (isGapWideEnoughToDrawLines(name, hashes[i])) {
+
+            drawLinesBetweenHashes(name, hashes, i, side)
+
         }
 
     }
+}
+
+function getHashes(side) {
+    return $(svg).find('.' + side + '-pitcher-hash').toArray()
+}
+
+function isNameWiderThanAdjacentHashMarks(name, hash) {
+    const BUFFER = 10
+    return name.y.baseVal[0].value - name.getBBox().width / 2 - BUFFER < hash.y1.baseVal.value
+}
+
+function getNewX(names, i, side) {
+    const shift = (side === 'home' ? 15 : -15)
+    let nameX = names[i].x.baseVal[0].value + shift
+    for (let j = 0; j < i; j++) {
+        const pName = names[j]
+        const xCollide = nameX === pName.x.baseVal[0].value
+        const yCollide = getBottom(pName) > getTop(names[i])
+
+        if (xCollide && yCollide)
+            nameX += shift
+    }
+    return nameX
+}
+
+function getTop(obj) {
+    return obj.y.baseVal[0].value - obj.getBBox().width / 2
+}
+
+function getBottom(obj) {
+    return obj.y.baseVal[0].value + obj.getBBox().width / 2
+}
+
+function shiftName(name, side, newX) {
+    $(name).attr('x', newX)
+    const rotation = (side === 'home' ? '90' : '-90')
+    const y = name.y.baseVal[0].value
+    $(name).attr('transform', 'rotate(' + rotation + ',' + newX + ',' + y + ')')
+}
+
+function isGapWideEnoughToDrawLines(name, hash) {
+    const BUFFER = 10 
+    return name.y.baseVal[0].value - name.getBBox().width / 2 - hash.y1.baseVal.value > BUFFER
+}
+
+function drawLinesBetweenHashes(name, hashes, i, side) {
+    const x = (hashes[i].x1.baseVal.value + hashes[i].x2.baseVal.value) / 2
+    let transform
+    if (side === 'home')
+        transform = 'translate(0, 0) scale(1, 1)'
+    else
+        transform = 'translate(600, 0) scale(-1, 1)'
+    const BUFFER = 5
+    const nameBox = name.getBBox()
+    const y1 = hashes[i].y1.baseVal.value
+    const y2 = name.y.baseVal[0].value - nameBox.width / 2 - BUFFER
+    appendLine(x, y1, x, y2, transform, 'team-box')
+    const y3 = name.y.baseVal[0].value + nameBox.width / 2 + BUFFER
+    const y4 = hashes[i + 1].y1.baseVal.value
+    appendLine(x, y3, x, y4, transform, 'team-box')
 }
 
 function appendLine(x1, y1, x2, y2, transform, className) {
@@ -86,5 +110,5 @@ function appendLine(x1, y1, x2, y2, transform, className) {
 }
 
 function SVG(tag) {
-        return document.createElementNS('http://www.w3.org/2000/svg', tag);
+    return document.createElementNS('http://www.w3.org/2000/svg', tag);
 }
