4c3e1466752806968a722bf61bb2936dd10b45ac
John Brewer <brewerja@gmail.com>
2018-10-16 11:49:36 -0400

    Add gzip

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sslify import SSLify
+from flask_compress import Compress
 
 app = Flask(__name__)
 sslify = SSLify(app, permanent=True)
+Compress(app)
 
 from app import routes
